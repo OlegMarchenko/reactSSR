ff499f42ef5a604ed12ce8c331d1eda65481fe16
o.marchenko <o.marchenko@nekwall.com>
2019-01-03 15:21:12 +0200

    ready to output popular repositories

--- src/server/index.js
+++ src/server/index.js
@@ -3,6 +3,8 @@ import cors from "cors"
 import { renderToString } from "react-dom/server"
 import App from '../shared/App'
 import React from 'react'
+import serialize from "serialize-javascript"
+import { fetchPopularRepos } from '../shared/api'
 
 const app = express();
 
@@ -14,22 +16,27 @@ app.use(cors());
 app.use(express.static("public"));
 
 app.get("*", (req, res, next) => {
-    const markup = renderToString(
-        <App />
-    );
+    fetchPopularRepos()
+        .then((data) => {
+            const markup = renderToString(
+                <App data={data} />
+            );
 
-    res.send(`
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <title>SSR with RR</title>
-        <script src="/bundle.js" defer></script>
-      </head>
-      <body>
-        <div id="app">${markup}</div>s
-      </body>
-    </html>
-  `)
+            res.send(`
+        <!DOCTYPE html>
+        <html>
+          <head>
+            <title>SSR with RR</title>
+            <script src="/bundle.js" defer></script>
+            <script>window.__INITIAL_DATA__ = ${serialize(data)}</script>
+          </head>
+
+          <body>
+            <div id="app">${markup}</div>
+          </body>
+        </html>
+      `)
+        })
 });
 
 process.on("uncaughtException", (err) => {
@@ -41,5 +48,5 @@ process.on("unhandledRejection", (reason) => {
 });
 
 app.listen(5000, () => {
-    console.log(`Server is listening on port: 3000`)
+    console.log(`Server is listening on port: 5000`)
 });
